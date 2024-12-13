6d42c281748898a19a71b393334b5a5e41591d11
Seleznev Mikhail <seleznev@airi.net>
2024-12-13 18:39:15 +0300

    Turn off batch calibration; add hack with variable batch size

--- launch.sh
+++ launch.sh
@@ -22,22 +22,27 @@ then
     echo "DISABLE_CHAT_TEMPLATE = 1 => FORCED NO CHAT TEMPLATE FOR ALL MODELS"
 fi
 
-for task in task050 task065 task069 task070 task114 task133 task155 task158 task161 task162 task163 task190 task213 task214 task220 task279 task280 task286 task296 task297 task316 task317 task319 task320 task322 task323 task325 task326 task327 task328 task335 task337 task385 task580 task607 task608 task609 task904 task905 task1186 task1283 task1284 task1297 task1347 task1387 task1419 task1420 task1421 task1423 task1502 task1612 task1678 task1724
+# A hack to speed up experiments: everything is first launched with large batch, some runs fall with OOM, but get re-launched in next iteration.
+# Results for finished tasks are not re-computed.
+for batch_size_llm in 64 32 16 8
 do
-    CUDA_VISIBLE_DEVICES=$devices python main.py \
-        --task_filename ${task}_ \
-        --dataset_name natural-instructions \
-        --num_formats_to_analyze ${num_formats_to_analyze} \
-        --batch_size_llm 8 \
-        --num_samples 1000 \
-        --model_name ${model} \
-        --n_shot 5 \
-        --num_ensembles 4 \
-        --ensemble_size 5 \
-        --apply_batch_calibration 1 \
-        --evaluation_metric probability_ranking \
-        --evaluation_type full \
-        --cache_dir /home/seleznev/.cache/huggingface \
-        --output_dir exp/${exp_name} \
-        --nodes_to_evaluate_filepath train_test_splits/compositional_separator_space/holistic_random_sample_${task}_nodes_${num_formats_to_analyze}_textdisabled.json
+    for task in task050 task065 task069 task070 task114 task133 task155 task158 task161 task162 task163 task190 task213 task214 task220 task279 task280 task286 task296 task297 task316 task317 task319 task320 task322 task323 task325 task326 task327 task328 task335 task337 task385 task580 task607 task608 task609 task904 task905 task1186 task1283 task1284 task1297 task1347 task1387 task1419 task1420 task1421 task1423 task1502 task1612 task1678 task1724
+    do
+        CUDA_VISIBLE_DEVICES=$devices python main.py \
+            --task_filename ${task}_ \
+            --dataset_name natural-instructions \
+            --num_formats_to_analyze ${num_formats_to_analyze} \
+            --batch_size_llm ${batch_size_llm} \
+            --num_samples 1000 \
+            --model_name ${model} \
+            --n_shot 5 \
+            --num_ensembles 4 \
+            --ensemble_size 5 \
+            --apply_batch_calibration 0 \
+            --evaluation_metric probability_ranking \
+            --evaluation_type full \
+            --cache_dir /home/seleznev/.cache/huggingface \
+            --output_dir exp/${exp_name} \
+            --nodes_to_evaluate_filepath train_test_splits/compositional_separator_space/holistic_random_sample_${task}_nodes_${num_formats_to_analyze}_textdisabled.json
+    done
 done
