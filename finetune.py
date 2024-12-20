6d25db939d5cc187ddbbc005bb9941564dfb4195
Seleznev Mikhail <seleznev@airi.net>
2024-12-20 12:49:36 +0300

    Support random (iid) format split in finetuning script; add comments

--- finetune.py
+++ finetune.py
@@ -1,10 +1,11 @@
 import ast
+import json
 import torch
 import random
 import pandas as pd
 from argparse import ArgumentParser
 from dataclasses import dataclass
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Set, Dict
 from datasets import load_dataset
 from trl import SFTTrainer
 from unsloth import FastLanguageModel, is_bfloat16_supported
@@ -13,11 +14,9 @@ from transformers import TrainingArguments, DataCollatorForSeq2Seq, DataCollator
 from datasets import Dataset
 
 from generate_train_val_test_formats import VANILLA_MAPPING_ALL_CATEGORIES, COMPOSITIONAL_TRAIN_SEPARATOR_LIST, COMPOSITIONAL_TRAIN_SPACE_LIST
-VANILLA_MAPPING_ALL_CATEGORIES["chosen_space"] = [(e, e) for e in COMPOSITIONAL_TRAIN_SPACE_LIST]
-VANILLA_MAPPING_ALL_CATEGORIES["chosen_separator"] = [(e, e) for e in COMPOSITIONAL_TRAIN_SEPARATOR_LIST]
 
 
-def load_model(name, max_seq_length, dtype, load_in_4bit, device):
+def load_model(name: str, max_seq_length: int, dtype: type, load_in_4bit: bool, device: str):
     model, tokenizer = FastLanguageModel.from_pretrained(
         model_name=name,
         max_seq_length=max_seq_length,
@@ -29,31 +28,95 @@ def load_model(name, max_seq_length, dtype, load_in_4bit, device):
     return model, tokenizer
 
 
-def get_hermes_dataset(dataset_name: str, n_samples: int = 8000, n_augmentations: int = 4):
-    seed = 23
-    random.seed(seed)
+def _get_format_hash(space_str: str, separator_str: str, text_descriptor_fn_str: str) -> int:
+    """Map a sequence of essential format elements to an integer.
+    """
+    format_str = space_str + separator_str + text_descriptor_fn_str
+    return hash(format_str)
+
+
+def augment_conversation(conversation, format_split_mode: str, test_format_hashes: Set[int] | None) -> Dict[str, str]:
+    """Apply a random training prompt format to a conversation.
+    """
+    mapping_all_categories = {key: value for key, value in VANILLA_MAPPING_ALL_CATEGORIES.items()}
+    if format_split_mode == "compositional_separator_space":
+        mapping_all_categories["chosen_space"] = [(e, e) for e in COMPOSITIONAL_TRAIN_SPACE_LIST]
+        mapping_all_categories["chosen_separator"] = [(e, e) for e in COMPOSITIONAL_TRAIN_SEPARATOR_LIST]
+    elif format_split_mode == "random":
+        pass
+    else:
+        raise NotImplementedError("Modes other than 'compositional_separator_space' and 'random' are not supported.")
+
     verbalizer_first_options = ["question", "query", "Q"]
     verbalizer_second_options = ["answer", "reply", "A"]
 
-    def augment_conversation(conversation):
-        filtered_conversation = [msg for msg in conversation if msg["from"] in ("human", "gpt")]
-        filtered_conversation = filtered_conversation[:2]
-        assert filtered_conversation[0]["from"] == "human"
-        assert filtered_conversation[1]["from"] == "gpt"
+    filtered_conversation = [msg for msg in conversation if msg["from"] in ("human", "gpt")]
+    filtered_conversation = filtered_conversation[:2]
+    assert filtered_conversation[0]["from"] == "human"
+    assert filtered_conversation[1]["from"] == "gpt"
 
-        current_text_descriptor_fn = random.choice(VANILLA_MAPPING_ALL_CATEGORIES["text_descriptor_fn"])[0]
-        _verbalizer_first = current_text_descriptor_fn(random.choice(verbalizer_first_options))
-        _verbalizer_second = current_text_descriptor_fn(random.choice(verbalizer_second_options))
-        _separator = random.choice(VANILLA_MAPPING_ALL_CATEGORIES["chosen_separator"])[0]
+    # For "random" format_split_mode, sample until we don't get a format which is not in test set:
+    while True:
+        _text_descriptor_fn, _text_descriptor_fn_str = random.choice(mapping_all_categories["text_descriptor_fn"])
+        _separator, _serarator_str = random.choice(mapping_all_categories["chosen_separator"])
         if "\n" in _separator:
-            _space = random.choice([e[0] for e in VANILLA_MAPPING_ALL_CATEGORIES["chosen_space"] if "\n" in e[0]])
+            _space, _space_str = random.choice([e for e in mapping_all_categories["chosen_space"] if "\n" in e[0]])
         else:
-            _space = random.choice(VANILLA_MAPPING_ALL_CATEGORIES["chosen_space"])[0]
+            _space, _space_str = random.choice(mapping_all_categories["chosen_space"])
 
-        text = f"{_verbalizer_first}{_separator}{filtered_conversation[0]['value']}{_space}"\
-               f"{_verbalizer_second}{_separator}{filtered_conversation[1]['value']}"
+        current_hash = _get_format_hash(_space_str, _serarator_str, _text_descriptor_fn_str)
+
+        if format_split_mode != "random" or current_hash not in test_format_hashes:
+            break
+        else:
+            print(f"Regenerating format", [_space, _separator, _text_descriptor_fn_str])
+
+    _text_descriptor_first = _text_descriptor_fn(random.choice(verbalizer_first_options))
+    _text_descriptor_second = _text_descriptor_fn(random.choice(verbalizer_second_options))
+    text = f"{_text_descriptor_first}{_separator}{filtered_conversation[0]['value']}{_space}"\
+            f"{_text_descriptor_second}{_separator}{filtered_conversation[1]['value']}"
+
+    return {"text": text}
+
+
+def _compose_test_hashes_set(format_split_mode: str, path_to_test_formats: str) -> Set[int] | None:
+    """If split mode is "random", computes hashes for test prompt formats.
+    Resulting set is used to efficiently check whether a random generated format matches with some test format.
+    """
+    if format_split_mode == "random":
+        with open(path_to_test_formats, "r") as f:
+            config = json.load(f)
         
-        return {"text": text}
+        test_format_hashes = set()
+        for format in config["test_formats"]:
+            space_idx = config["action_types"].index("chosen_space")
+            sep_idx = config["action_types"].index("chosen_separator")
+            text_descriptor_fn_idx = config["action_types"].index("text_descriptor_fn")
+
+            test_format_hashes.add(
+                _get_format_hash(format[space_idx], format[sep_idx], format[text_descriptor_fn_idx])
+            )
+    else:
+        # For all other modes, leakage is avoided in a different way,
+        # through patching the set of possible choices for format elements.
+        test_format_hashes = None
+
+    return test_format_hashes
+
+
+def get_dataset(dataset_name: str, n_samples: int, n_augmentations: int, format_split_mode: str, path_to_test_formats: str) -> Dataset:
+    """Load a dataset.
+    Args:
+        dataset_name: path to .csv file or a huggingface dataset name.
+        n_samples: size of a subsample for huggingface datasets.
+        n_augmentations: for each example from (a subsample of) original dataset, `n_augmentation` versions of it with different formats will be added to the final dataset.
+        format_split_mode: defines which augmentations can be applied -- we can only use formats from train split.
+        path_to_test_formats: for format_split_mode = 'random', we need a path to a complete list test formats.
+    Output:
+        final_dataset: dataset with augmented versions of original samples.
+    """
+    seed = 23
+    random.seed(seed)
 
     print(dataset_name)
     if dataset_name.endswith(".csv"):
@@ -64,10 +127,12 @@ def get_hermes_dataset(dataset_name: str, n_samples: int = 8000, n_augmentations
         dataset = dataset.shuffle(seed=seed)
         dataset = dataset.select(range(min(n_samples, len(dataset))))
 
+    test_format_hashes = _compose_test_hashes_set(format_split_mode, path_to_test_formats)
+
     all_formatted_texts = []
     for conversation in dataset["conversations"]:
         for _ in range(n_augmentations):
-            all_formatted_texts.append(augment_conversation(conversation))
+            all_formatted_texts.append(augment_conversation(conversation, format_split_mode, test_format_hashes))
 
     final_dataset = Dataset.from_list(all_formatted_texts)
 
@@ -90,9 +155,17 @@ class LoraArguments:
     use_rslora: bool = False
 
 
-def run_finetuning(model_name: str, dataset_name: str, lora_arguments: LoraArguments, training_arguments: TrainingArguments,
-                   seed: int, max_seq_length: int, dtype: torch.dtype, load_in_4bit: bool, device: str, output_dir: str,
-                   n_original_samples: int, n_augmentations: int):
+@dataclass
+class DatasetArguments:
+    dataset_name: str
+    n_original_samples: int
+    n_augmentations: int
+    format_split_mode: str
+    path_to_test_formats: str
+
+
+def run_finetuning(model_name: str, lora_arguments: LoraArguments, training_arguments: TrainingArguments, dataset_arguments: DatasetArguments,
+                   seed: int, max_seq_length: int, dtype: torch.dtype, load_in_4bit: bool, device: str, output_dir: str):
     model, tokenizer = load_model(model_name, max_seq_length, dtype, load_in_4bit, device)
 
     model = FastLanguageModel.get_peft_model(
@@ -109,7 +182,13 @@ def run_finetuning(model_name: str, dataset_name: str, lora_arguments: LoraArgum
         loftq_config=None, # And LoftQ
     )
 
-    dataset = get_hermes_dataset(dataset_name, n_original_samples, n_augmentations)
+    dataset = get_dataset(
+        dataset_arguments.dataset_name,
+        dataset_arguments.n_original_samples,
+        dataset_arguments.n_augmentations,
+        dataset_arguments.format_split_mode,
+        dataset_arguments.path_to_test_formats
+    )
 
     trainer = SFTTrainer(
         model=model,
@@ -180,9 +259,13 @@ def make_parser():
     parser.add_argument("--max-seq-length", type=int, default=2048)
     parser.add_argument("--load-in-4bit", action="store_true")
     parser.add_argument("--device", type=str, default="cuda:0")
+
+    # Dataset arguments
     parser.add_argument("--n-original-samples", type=int, default=8000)
     parser.add_argument("--n-augmentations", type=int, default=4)
     parser.add_argument("-d", "--dataset-name", default="teknium/OpenHermes-2.5")
+    parser.add_argument("--format-split-mode", required=True)
+    parser.add_argument("--path-to-test-formats")
 
     # LoRA arguments
     parser.add_argument("--lora-rank", type=int, default=16)
@@ -191,7 +274,7 @@ def make_parser():
     parser.add_argument("--use-rslora", action="store_true")
 
     # Training arguments
-    parser.add_argument("-b", "--batch-size", type=int, default=8)
+    parser.add_argument("-b", "--batch-size", type=int, default=16)
     parser.add_argument("--grad-accum-steps", type=int, default=4)
     parser.add_argument("--warmup-steps", type=int, default=5)
     parser.add_argument("--num-epochs", type=int, default=1)
@@ -222,6 +305,14 @@ if __name__ == "__main__":
         use_rslora=args.use_rslora,
     )
 
+    dataset_arguments = DatasetArguments(
+        dataset_name=args.dataset_name,
+        n_original_samples=args.n_original_samples,
+        n_augmentations=args.n_augmentations,
+        format_split_mode=args.format_split_mode,
+        path_to_test_formats=args.path_to_test_formats
+    )
+
     training_arguments = TrainingArguments(
         per_device_train_batch_size=args.batch_size,
         gradient_accumulation_steps=args.grad_accum_steps,
@@ -242,8 +333,7 @@ if __name__ == "__main__":
         save_steps=100,
     )
 
-    model, tokenizer = run_finetuning(args.model_name, args.dataset_name, lora_arguments, training_arguments, args.seed,
-                                      args.max_seq_length, dtype, args.load_in_4bit, args.device, args.output_dir,
-                                      args.n_original_samples, args.n_augmentations)
+    model, tokenizer = run_finetuning(args.model_name, lora_arguments, training_arguments, dataset_arguments, args.seed,
+                                      args.max_seq_length, dtype, args.load_in_4bit, args.device, args.output_dir)
 
     sample_inference(model, tokenizer)
