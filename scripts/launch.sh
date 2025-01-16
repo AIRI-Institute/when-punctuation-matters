69cf5fff5eb4c932262436e3636020eb98d212f3
Seleznev Mikhail <seleznev@airi.net>
2025-01-16 14:43:52 +0300

    Exclude task 190 from evals -- it had erroneous labels

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -34,7 +34,8 @@ fi
 # Results for finished tasks are not re-computed.
 for batch_size_llm in 64 32 16 8
 do
-    for task in task050 task065 task069 task070 task114 task133 task155 task158 task161 task162 task163 task190 task213 task214 task220 task279 task280 task286 task296 task297 task316 task317 task319 task320 task322 task323 task325 task326 task327 task328 task335 task337 task385 task580 task607 task608 task609 task904 task905 task1186 task1283 task1284 task1297 task1347 task1387 task1419 task1420 task1421 task1423 task1502 task1612 task1678 task1724
+    # task190 excluded due to errors in labels
+    for task in task050 task065 task069 task070 task114 task133 task155 task158 task161 task162 task163 task213 task214 task220 task279 task280 task286 task296 task297 task316 task317 task319 task320 task322 task323 task325 task326 task327 task328 task335 task337 task385 task580 task607 task608 task609 task904 task905 task1186 task1283 task1284 task1297 task1347 task1387 task1419 task1420 task1421 task1423 task1502 task1612 task1678 task1724
     do
         CUDA_VISIBLE_DEVICES=$devices python main.py \
             --task_filename ${task}_ \
